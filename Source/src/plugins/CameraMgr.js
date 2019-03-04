d34675ac8ffbafe81eeb5a5a3f4c1fe2ea2ebeef
alucard263096 <alucard263096@126.com>
2019-03-04 18:34:26 +0800

    commit

--- Source/src/plugins/CameraMgr.js
+++ Source/src/plugins/CameraMgr.js
@@ -1,9 +1,10 @@
 export class CameraMgr {
   static getPicture(onSuccess, onFail) {
-    navigator.camera.getPicture(onSuccess, onFail, {
-      destinationType: Camera.DestinationType.FILE_URI,
-      sourceType: Camera.PictureSourceType.PHOTOLIBRARY,
-      popoverOptions: new CameraPopoverOptions(300, 300, 100, 100, Camera.PopoverArrowDirection.ARROW_ANY)
-    });
+      alert(1);
+    // navigator.camera.getPicture(onSuccess, onFail, {
+    //   destinationType: Camera.DestinationType.FILE_URI,
+    //   sourceType: Camera.PictureSourceType.PHOTOLIBRARY,
+    //   popoverOptions: new CameraPopoverOptions(300, 300, 100, 100, Camera.PopoverArrowDirection.ARROW_ANY)
+    // });
   }
 }
